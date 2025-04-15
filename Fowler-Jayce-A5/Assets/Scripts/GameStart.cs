7d5f311d3573805489276f8e7737c6977891c986
Fowlerpro <jaycefowler@outlook.com>
2025-04-15 18:28:39 -0400

    add pause and start combined

--- Fowler-Jayce-A5/Assets/Scripts/GameStart.cs
+++ Fowler-Jayce-A5/Assets/Scripts/GameStart.cs
@@ -3,22 +3,53 @@ using UnityEngine;
 public class GameStart : MonoBehaviour
 {
     public GameObject startScreen;
+    public GameObject pauseScreen;
     public bool isStarted = true;
+    public bool isPaused = false;
+    [SerializeField] KeyCode resume = KeyCode.P;
     [SerializeField] KeyCode start = KeyCode.Space;
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (isStarted)
+        Pause();
+        if (isStarted == true)
         {
             Time.timeScale = 0f;
+            if (Input.GetKey(start))
+            {
+                isStarted = false;
+                startScreen.SetActive(false);
+                Time.timeScale = 1f;
+            }
         }
-        if (Input.GetKey(start))
+        void Pause()
         {
-            isStarted = false;
-         startScreen.SetActive(false);
-            Time.timeScale = 1f;
+            if (Input.GetKeyDown(resume))
+            {
+                isPaused = !isPaused;
+
+            }
+            if (isPaused)
+            {
+                AddMenu();
+                Time.timeScale = 0f;
+
+            }
+            else
+            {
+                RemoveMenu();
+                Time.timeScale = 1f;
+            }
         }
+            void AddMenu()
+        {
+            pauseScreen.SetActive(true);
+        }
+        void RemoveMenu()
+        {
+            pauseScreen.SetActive(false);
+        }
+
     }
 }
--- Fowler-Jayce-A5/Assets/Scripts/Pause.cs
+++ Fowler-Jayce-A5/Assets/Scripts/Pause.cs
@@ -4,34 +4,4 @@ public class Pause : MonoBehaviour
 {
     public GameObject pauseScreen;
     public bool isPaused;
-    [SerializeField] KeyCode resume = KeyCode.P;
-    // Update is called once per frame
-    void Update()
-    {
-       if (Input.GetKeyDown(resume))
-        {
-            isPaused = !isPaused;
-
-        }
-        if (isPaused)
-        {
-            AddMenu();
-            Time.timeScale = 0f;
-
-        }
-        else
-        {
-            RemoveMenu();
-            Time.timeScale = 1f;
-        }
-    }
-    void AddMenu()
-    {
-        pauseScreen.SetActive(true);
-    }
-    void RemoveMenu()
-    {
-        pauseScreen.SetActive(false);
-    }
-
-}
+   
